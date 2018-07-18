77690d14a335d18d7c43764bb2cd141de5bc8855
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-18 12:00:57 -0600

    Added more content to milestone-1.php.

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -23,10 +23,17 @@
 			<div>
 				<p><span class="wrap">Goals:</span> The initial goal of this site is to draw attention of recruiters/ hiring managers and potential employers to my skills and qualifications and initiate contact with me through the provided contact form or by connecting with me through social media. The secondary goal of this site is to expand my professional online presence. Finally, the ultimate goal of this site is to find a full-time job as a web developer.</p>
 			</div>
+			<h2>Persona</h2>
 			<div>
-				<h2></h2>
-				<p></p>
+				<p><span class="wrap2">Name:</span> Alison J. Cook</p>
 			</div>
+			<div>
+				<p><span class="wrap2">Age:</span> 43</p>
+			</div>
+			<div>
+				<p><span class="wrap2">Profession:</span> Alison is a hiring manager of a medium sized software development company. Currently, Alison is actively seeking potential candidates to fill two new junior web developer positions at the company. In addition to the list of specific professional requirements, Alison was instructed to look for people with a good aesthetic/design sense that company’s team of web developers needs badly at the present.</p>
+			</div>
+
 
 		</main>
 		<footer>
