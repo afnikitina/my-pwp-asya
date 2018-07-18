d398056cd85a719809fb873e37837e5fe7a5da05
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-18 10:39:29 -0600

    Created a first draft of the web page.

---
+++ documentation/milestone-1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Asya Nikitina - Personal Website Project</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="./styles/style.css">
+	</head>
+	<body>
+		<header>
+			<div id="firstDiv">
+			<p>Welcome! I'm<p>
+			</div>
+			<h1>Asya Nikitina</h1>
+		</header>
+		<main>
+			<h2>Purpose, Audience & Goals</h2>
+			<div>
+				<h2></h2>
+				<p></p>
+			</div>
+			<div>
+				<h2></h2>
+				<p></p>
+			</div>
+			<div>
+				<h2></h2>
+				<p></p>
+			</div>
+			<div>
+				<h2></h2>
+				<p></p>
+			</div>
+
+		</main>
+		<footer>
+			Copyright &copy; 2018 Asya Nikitina
+		</footer>
+	</body>
+</html>
