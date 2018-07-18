94e7b052e2e19c867c67bd92e95462a85542151f
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-18 14:54:17 -0600

    Modified some of the content of milestone-1.php.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # my-pwp-asya
-This is a Personal Website Project that outlines my web development education, as well as personal and professional growth. The ultimate goal of working on this project is to attract potential employers and/or recruiters/hiring managers and find a full-time job as a web developer.
+This is a ‘Personal Website Project’ that outlines my web development education, as well as personal and professional growth. Upon completing this project, I hope to attract the attention of potential employers and/or recruiters/hiring managers and find a full-time job as a web developer.
---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Asya Nikitina - Personal Website Project</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+	</head>
+	<body>
+		<header>
+			<div id="firstDiv">
+			<p>Welcome! I'm<p>
+			<h1>Asya Nikitina</h1>
+			</div>
+		</header>
+		<main>
+			<h2>Purpose, Audience & Goals</h2>
+			<div>
+				<p><span class="wrap">Purpose:</span> Create a professional portfolio website with the use of various front-end web development technologies.</p>
+			</div>
+			<div>
+				<p><span class="wrap">Target Audience:</span> The site is designed to be viewed by web developers, IT professionals, as well as startup founders, recruiters, and hiring managers who are actively looking for potential candidates among web developers.</p>
+			</div>
+			<div>
+				<p><span class="wrap">Goals:</span> The initial goal of this site is to draw attention of recruiters/ hiring managers and potential employers to my skills and qualifications and initiate contact with me through the provided contact form or by connecting with me through social media. The secondary goal of this site is to expand my professional online presence. Finally, the ultimate goal of this site is to find a full-time job as a web developer.</p>
+			</div>
+			<h2>Persona</h2>
+			<div>
+				<p><span class="wrap2">Name:</span> Alison J. Cook</p>
+			</div>
+			<div>
+				<p><span class="wrap2">Age:</span> 43</p>
+			</div>
+			<div>
+				<p><span class="wrap2">Profession:</span> Alison is a hiring manager of a medium sized software development company. Currently, Alison is actively seeking potential candidates to fill two new junior web developer positions at the company. In addition to the list of specific professional requirements, Alison was instructed to look for people with a good aesthetic/design sense that company’s team of web developers needs badly at the moment.</p>
+			</div>
+			<div>
+				<p><span class="wrap2">Technology:</span> Alison uses a Windows Surface Pro (Intel Core i7; 16GB RAM; 512GB SSD; 12.3" Touchscreen 2736x1824px Display; Windows 10 Pro) at work and a Dell Inspiron 17 5000 Series laptop (Intel Core i7; 16GB RAM; 17.3" 1920x1080px Display; Windows 10 Home) at home. She also owns an  Apple iPhone 5S (iOS 7; 16GB Internal Storage; 1136x640px resolution) with an unlimited 4G data connection.</p>
+			</div>
+			<div>
+				<p><span class="wrap2">Attitudes and Behaviors:</span> Alison has five weeks to find suitable candidates for the new junior web developer positions. Ideally, she wants to prescreen potential candidates by looking at theirs professional personal websites in addition to their resumes. Since visual appeal of a personal website is one of Alison’s objectives, she is willing to spend an extra few minutes looking at personal web pages of potential candidates both in terms of professional content and aesthetic look and feel.</p>
+			</div>
+			<div>
+				<p><span class="wrap2">Frustrations and Needs:</span> Alison doesn’t like when potential candidates for open positions at her company create professional portfolio websites without spending enough time and efforts, or without using a carefully thought-out design. What frustrates Alison even more is when people present inaccurate information on their professional portfolio web pages.<br>Currently, she needs to hire two junior web developers with strong background in both front and end web development, substantial hands-on experience in modern web technologies, and who also have a good aesthetic sense.</p>
+			</div>
+			<div>
+				<p><span class="wrap2">Goals:</span> At this stage of the hiring process, Alison wants to find a few potential candidates for junior web developer positions for the initial phone interviews. The professional and technical skills of those candidates should exactly match the job description and their professional portfolio websites must be both professionally and aesthetically appealing.</p>
+			</div>
+			<div>
+				<p><span class="wrap2">User Story:</span> As a user, I need to determine a few potential candidates for two junior web developer positions so I can continue evaluating them using more specific criteria. Even though this is just an initial prescreening process, both professional and aesthetic look and feel of personal websites are very important to me.</p>
+			</div>
+		</main>
+		<footer>
+			Copyright &copy; 2018 Asya Nikitina
+		</footer>
+	</body>
+</html>
