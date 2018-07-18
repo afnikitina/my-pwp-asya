7e31d14bee910f898966ccc3ae70ce5d5879d8ab
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-18 11:26:37 -0600

    Modified the content of milestone-1.php.

--- documentation/milestone-1.php
+++ documentation/milestone-1.php
@@ -9,22 +9,19 @@
 		<header>
 			<div id="firstDiv">
 			<p>Welcome! I'm<p>
-			</div>
 			<h1>Asya Nikitina</h1>
+			</div>
 		</header>
 		<main>
 			<h2>Purpose, Audience & Goals</h2>
 			<div>
-				<h2></h2>
-				<p></p>
+				<p><span class="wrap">Purpose:</span> Create a professional portfolio website with the use of various front-end web development technologies.</p>
 			</div>
 			<div>
-				<h2></h2>
-				<p></p>
+				<p><span class="wrap">Target Audience:</span> The site is designed to be viewed by web developers, IT professionals, as well as startup founders, recruiters, and hiring managers who are actively looking for potential candidates among web developers.</p>
 			</div>
 			<div>
-				<h2></h2>
-				<p></p>
+				<p><span class="wrap">Goals:</span> The initial goal of this site is to draw attention of recruiters/ hiring managers and potential employers to my skills and qualifications and initiate contact with me through the provided contact form or by connecting with me through social media. The secondary goal of this site is to expand my professional online presence. Finally, the ultimate goal of this site is to find a full-time job as a web developer.</p>
 			</div>
 			<div>
 				<h2></h2>
