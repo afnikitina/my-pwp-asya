89396c5534b9f01b5dd22d357e551c7da577f0b0
Asya Nikitina <asya_nikitina@hotmail.com>
2018-08-01 00:17:05 -0600

    Added milestone-2.php and mobile-layout.png files

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Personal Website Project - Content Strategy</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+	</head>
+	<body>
+		<h2>Wireframes</h2>
+		<h3>Mobile Layout</h3>
+		<img src="./images/mobile-layout.png" alt="Wireframes Mobile Layout">
+
+	</body>
+</html>
