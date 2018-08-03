5c060478171d2fa761b1d412574659c4376362ba
Asya Nikitina <anikitina@cnm.edu>
2018-08-02 22:24:39 -0600

    Changed images in milesone-2.php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -3,12 +3,15 @@
 	<head>
 		<title>Personal Website Project - Content Strategy</title>
 		<meta charset="UTF-8" />
-		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<link rel="stylesheet" type="text/css" href="styles/style.css" />
 	</head>
 	<body>
-		<h2>Wireframes</h2>
-		<h3>Mobile Layout</h3>
-		<img src="./images/mobile-layout.png" alt="Wireframes Mobile Layout">
-
+		<div class="layout">
+			<h2>Wireframes</h2>
+			<h3>Mobile Layout</h3>
+			<img src="./images/mobile-layout1.png" alt="Wireframes Mobile Layout">
+			<h3>Desktop Layout</h3>
+			<img src="./images/desktop-layout1.png" alt="Wireframes Desktop Layout">
+		</div>
 	</body>
 </html>
