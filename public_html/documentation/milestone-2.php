6afe2a2fc8964b9b92f45869389f394520a6524d
Asya Nikitina <anikitina@cnm.edu>
2018-08-05 20:00:57 -0600

    Added description of my content stratege to milesone-2.php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,12 +6,37 @@
 		<link rel="stylesheet" type="text/css" href="styles/style.css" />
 	</head>
 	<body>
-		<div class="layout">
-			<h2>Wireframes</h2>
-			<h3>Mobile Layout</h3>
-			<img src="./images/mobile-layout1.png" alt="Wireframes Mobile Layout">
-			<h3>Desktop Layout</h3>
-			<img src="./images/desktop-layout1.png" alt="Wireframes Desktop Layout">
-		</div>
+		<header>
+			<div id="firstDiv">
+				<h1>Content Strategy</h1>
+			</div>
+		</header>
+		<main>
+			<p>My site will be one page, and will consist of the following sections:</p>
+			<section>
+				<p><span class="wrap">About Me:</span> This section will consist of a welcome message, which will be overlaid on a large background image. The background image will be bright and abstract and on the top of it on the right side there will be a small image of me. Under this welcome image, there will be a brief introduction that clearly states my name and role as a web developer.</p>
+				<p>Below that, there will be two separate sections laid out horizontally: the “Education” section will be on the left and the “Technical Skills” section will be on the right. Each section will contain a few lines of text. I will try to highlight the set of skills and programming languages that are most relevant to my dream job that I will be seeking after graduation from the Deep Dive Bootcamp.</p>
+			</section>
+			<section>
+				<p><span class="wrap">Portfolio:</span> I will have two examples of my recent work as a web developer each will be displayed as an image and a brief description to it. Upon clicking on each image, a pop-up will appear that briefly describes a project and my role and contribution to it. One of these examples will be our capstone project (the Lost Paws app).</p>
+			</section>
+			<section>
+				<p><span class="wrap">Contact Me:</span> This section will contain a simple contact form with standard input fields such as Name, Email, Subject, and Message, and two buttons, Submit and Reset. The <strong>Contact Me</strong> form will be protected by reCAPTCHA.</p>
+			</section>
+			<section>
+				<p>The footer of the web page will contain two icons: LinkedIn and GitHub, which will be linked to my LinkedIn and GitHub accounts.</p>
+			</section>
+			<div class="layout">
+				<h2>Wireframes</h2>
+				<p>My site will feature one layout that will be fully responsive on small and large screens. These wireframes were created using <strong>https://balsamiq.com</strong></p>
+				<h3>Mobile Layout</h3>
+				<img src="./images/mobile-layout1.png" alt="Wireframes Mobile Layout">
+				<h3>Desktop Layout</h3>
+				<img src="./images/desktop-layout1.png" alt="Wireframes Desktop Layout">
+			</div>
+		</main>
+		<footer>
+			Copyright &copy; 2018 Asya Nikitina
+		</footer>
 	</body>
 </html>
