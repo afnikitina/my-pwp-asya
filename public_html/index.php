99cc775061f13c13fec2ef98409de453f5162187
Asya Nikitina <anikitina@cnm.edu>
2018-08-22 16:55:07 -0600

    Added the navigation bar.

--- public_html/index.php
+++ public_html/index.php
@@ -8,10 +8,52 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 		<link rel="stylesheet" href="css/style.css">
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 	</head>
 	<body>
+		<div class="container">
+			<ul class="nav nav-inline">
+				<li class="nav-item"><a href="#" class="nav-link active">About Me</a></li>
+				<li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Portfolio</a>
+					<div class="dropdown-menu">
+						<a href="#" class="dropdown-item">Project 1</a>
+						<a href="#" class="dropdown-item">Project 2</a>
+					</div>
+				</li>
+				<li class="nav-item"><a href="#" class="nav-link active">Contact Me</a></li>
+			</ul>
+		</div><!--		container-->
+		<div class="container">
+			<nav class="navbar">
+				<div class="nav navbar-nav">
+
+				</div>
+
+
+
+			</nav>
+
+
+
+
+			<ul class="nav nav-inline">
+				<li class="nav-item"><a href="#" class="nav-link active">About Me</a></li>
+				<li class="nav-item dropdown"><a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Portfolio</a>
+					<div class="dropdown-menu">
+						<a href="#" class="dropdown-item">Project 1</a>
+						<a href="#" class="dropdown-item">Project 2</a>
+					</div>
+				</li>
+				<li class="nav-item"><a href="#" class="nav-link active">Contact Me</a></li>
+			</ul>
+		</div><!--		container-->
 
 		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h2 class="text-center">Contact Me</h2>
+				</div><!--		col-md-6-->
+			</div><!--		row-->
 			<div class="row">
 				<div class="col-md-6">
 					<label class="control-label" for="name">Name <span class="red-star">*</span></label>
@@ -68,6 +110,9 @@
 <!--					<small id="subjectHelp" class="form-text text-muted">Please type your message in the box above.</small>-->
 				</div><!--		col-md-6-->
 			</div><!--		row-->
+			<div class="g-recaptcha" data-sitekey="6Lcxg2sUAAAAAJZdNwYeK8eHm4XygGXoF6eO62kt"></div>
+			<button type="button" class="btn btn-primary" onclick="submitMessage()"><div class="my-btn" style="text-align:left;"><i class="fa fa-paper-plane" style="color:white;" aria-hidden="true"></i>&nbsp;&nbsp;Send</div></button>
+			<button type="button" class="btn btn-warning" onclick="resetAll()"><div class="my-btn" style="text-align:left;"><i class="fa fa-undo" style="color:white;" aria-hidden="true"></i>&nbsp;&nbsp;Reset</div></button>
 		</div><!--		container-->
 
 
