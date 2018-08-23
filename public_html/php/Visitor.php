43baf77e6574096a45571a08168f0d77daeed410
Asya Nikitina <anikitina@cnm.edu>
2018-08-23 13:36:04 -0600

    Created a Visitor class.

---
+++ public_html/php/Visitor.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Message class describes a message created by the visitor of my web page
+ *
+ *
+ * @author Asya Nikitina <a.f.nikitina@gmail.com>
+ * @version 1.0.0
+ **/
+// create a Message class
+class Visitor {
+	private visitoName;
+	private visitorEmail;
+	private visitorSubject;
+	private visitorMessage;
+
+
+}
