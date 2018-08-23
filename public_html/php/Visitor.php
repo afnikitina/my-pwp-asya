0031f8150128bef65d7cd41a260fbcd5e682e6c7
Asya Nikitina <anikitina@cnm.edu>
2018-08-23 15:10:00 -0600

    Continue writing Visitor class in Visitor.php.

--- public_html/php/Visitor.php
+++ public_html/php/Visitor.php
@@ -9,7 +9,7 @@ namespace anikitina\MyPwpAsya;
  * @version 1.0.0
  **/
 // create a Message class
-class Visitor {
+class Visitor implements \JsonSerializable {
 
 	// declare private state variables
 	private $visitorEmail;
@@ -94,35 +94,82 @@ class Visitor {
 	 * accessor method for a name
 	 *
 	 *
-	 * @return string value of visitor's message
+	 * @return string value of visitor's name
 	 **/
-	public function getVisitorMessage(): string {
-		return $this->visitorMessage;
+	public function getVisitorName(): string {
+		return $this->visitorName;
 	}
 
 	/**
-	 * mutator method for setting/changing visitor's message
+	 * mutator method for setting/changing visitor's name
 	 *
-	 * @param string $newVisitorMessage new value of an email address
-	 * @throws \InvalidArgumentException if $newVisitorMessage is insecure
-	 * @throws \RangeException if $newVisitorMessage is > 1000 characters
+	 * @param string $newVisitorName new value of an email address
+	 * @throws \InvalidArgumentException if $newVisitorName is insecure
+	 * @throws \RangeException if $newVisitorName is > 92 characters
 	 **/
-	public function setVisitorMessage(string $newVisitorMessage): void {
+	public function setVisitorMessage(string $newVisitorName): void {
 		// verify that the message is secure and not empty
-		$newVisitorMessage = trim($newVisitorMessage);
-		$newVisitorMessage = filter_var($newVisitorMessage, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		$newVisitorName = trim($newVisitorName);
+		$newVisitorName = filter_var($newVisitorName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-		if(empty($newVisitorMessage)) {
+		if(empty($newVisitorName)) {
 			throw(new \InvalidArgumentException("Your message is empty or insecure."));
 		}
-		// verify that the message is not longer than 1000 characters
-		if(strlen($newVisitorMessage) > 1000) {
-			throw(new \RangeException("Message is too long."));
+		// verify that the message is not longer than 92 characters
+		if(strlen($newVisitorName) > 92) {
+			throw(new \RangeException("Name is too long."));
 		}
 		// store the valid email address
-		$this->visitorMessage = $newVisitorMessage;
+		$this->visitorName = $newVisitorName;
+	}
+
+	/**
+	 * accessor method for a subject
+	 *
+	 *
+	 * @return string value of visitor's subject
+	 **/
+	public function getVisitorSubject(): string {
+		return $this->visitorSubject;
+	}
+
+	/**
+	 * mutator method for setting/changing visitor's name
+	 *
+	 * @param string $newVisitorSubject new value of an email address
+	 * @throws \InvalidArgumentException if $newVisitorSubject is insecure
+	 * @throws \RangeException if $newVisitorSubject is > 100 characters
+	 **/
+	public function setVisitorSubject(string $newVisitorSubject): void {
+		// verify that the message is secure and not empty
+		$newVisitorSubject = trim($newVisitorSubject);
+		$newVisitorSubject = filter_var($newVisitorSubject, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+		if(empty($newVisitorSubject)) {
+			throw(new \InvalidArgumentException("Your message is empty or insecure."));
+		}
+		// verify that the message is not longer than 100 characters
+		if(strlen($newVisitorSubject) > 100) {
+			throw(new \RangeException("Name is too long."));
+		}
+		// store the valid email address
+		$this->visitorName = $newVisitorSubject;
 	}
 
 
-
+	/**
+	 * formats the state variables for JSON serialization
+	 *
+	 * @return array resulting state variables to serialize
+	 **/
+	public function jsonSerialize() {
+		// keep this profileAccessToken secret
+		// also we don't need profileOAuthId -- keep it out as well
+		return [
+			'visitorEmail' => $this->visitorEmail,
+			'visitorMessage' => $this->visitorMessage,
+			'visitorName' => $this->visitorName,
+			'visitorSubject' => $this->visitorSubject
+		];
+	}
 }
