f5d34b0db5bffda3201076749ab13531c282810e
Asya Nikitina <anikitina@cnm.edu>
2018-08-27 21:13:45 -0600

    Fixed the "contact me" form  and the footer.

--- public_html/index.php
+++ public_html/index.php
@@ -7,7 +7,7 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-<!--		<link href="https://fonts.googleapis.com/css?family=Handlee|Open+Sans" rel="stylesheet">-->
+		<!--		<link href="https://fonts.googleapis.com/css?family=Handlee|Open+Sans" rel="stylesheet">-->
 		<link href="https://fonts.googleapis.com/css?family=Acme|Handlee|Open+Sans" rel="stylesheet">
 		<link rel="stylesheet" href="css/style.css">
 		<script src='https://www.google.com/recaptcha/api.js'></script>
@@ -45,7 +45,7 @@
 					<span class="mr-3"><a class="bootstrap-link my-jumbotron" href="https://github.com/afnikitina"><i class="fab fa-github-square"></i></a></span>
 					<span class="mr-3"><a class="bootstrap-link my-jumbotron" href="https://github.com/afnikitina"><i class="fab fa-linkedin"></i></a></span>
 					<span><a class="bootstrap-link my-jumbotron" href="#"><i class="fas fa-envelope"></i></a></span>
-					</p>
+				</p>
 			</div>
 		</header>
 		<main>
@@ -119,80 +119,80 @@
 			</section><!--container-->
 
 			<!--Contact Me Form-->
-		<section class="container contact-form">
-			<div class="row justify-content-md-center">
-				<div class="col-md-6">
-					<h2 class="text-center">Contact Me</h2>
-				</div><!--col-md-6-->
-			</div><!--row justify-content-md-center-->
-			<form class="form-control-lg" id="form-contact" action="" method="post">
-				<div class="row justify-content-md-center">
-					<div class="col-md-6">
-						<label class="control-label" for="name">Name <span class="text-danger">*</span></label>
-						<div class="input-group mb-3">
-							<div class="input-group-prepend">
-								<span class="input-group-text"><i class="fas fa-user text-primary"></i></span>
-							</div><!--input-group-prepend-->
-							<input type="text" class="form-control" id="name" placeholder="Please enter your first and last name." name="name" required />
-						</div><!--input-group mb-3-->
-					</div><!--col-md-6-->
-				</div><!--row justify-content-md-center-->
-				<div class="row justify-content-md-center">
-					<div class="col-md-6">
-						<div class="form-group mb-3">
-							<label class="control-label" for="email">Email <span class="text-danger">*</span></label>
-							<div class="input-group">
-								<div class="input-group-prepend">
-									<span class="input-group-text"><i class="fas fa-envelope text-primary"></i></span>
-								</div><!--input-group-prepend-->
-								<input type="text" class="form-control" placeholder="Please enter your email address." name="email" aria-describedby="emailHelp" required />
-							</div><!--	input-group-->
-							<small id="emailHelp" class="form-text text-muted">I'll never share your email address with anyone else.</small>
-						</div><!--	form-group mb-3-->
-					</div><!--col-md-6-->
-				</div><!--row justify-content-md-center-->
-				<div class="row justify-content-md-center">
-					<div class="col-md-6">
-						<label class="control-label" for="subject">Subject</label>
-						<div class="input-group mb-3">
-							<div class="input-group-prepend">
-								<span class="input-group-text"><i class="fas fa-tag text-primary"></i></span>
-							</div>
-							<input type="text" class="form-control" placeholder="You may enter a subject of your message (optional)." name="subject" />
-						</div><!--input-group mb-3-->
-					</div><!--col-md-6-->
-				</div><!--row justify-content-md-center-->
-				<div class="row justify-content-md-center">
-					<div class="col-md-6">
-						<label class="control-label" for="message">Message <span class="text-danger">*</span></label>
-						<div class="input-group mb-3">
-							<div class="input-group-prepend">
-								<span class="input-group-text"><i class="fas fa-comment text-primary"></i></span>
-							</div>
-							<textarea class="form-control" rows="5" placeholder="Please enter your message here." name="message" required></textarea>
-						</div><!--input-group mb-3-->
-					</div><!--col-md-6-->
-				</div><!--row justify-content-md-center-->
-				<div class="row justify-content-md-center">
-					<div class="col-md-6">
-						<div class="g-recaptcha" data-sitekey="6Lcxg2sUAAAAAJZdNwYeK8eHm4XygGXoF6eO62kt"></div>
-					</div><!--col-md-6-->
-				</div><!--row justify-content-md-center-->
-				<div class="row justify-content-md-center">
-					<div class="col-md-6">
-						<button type="button" class="btn btn-primary text-light my-3 mr-3 contact-btn"><i class="fas fa-paper-plane"></i>&nbsp;&nbsp;Send</button>
-						<button type="button" class="btn btn-warning text-light my-3 contact-btn" onclick="resetAll()"><i class="fas fa-undo"></i>&nbsp;&nbsp;Reset</button>
-					</div><!--col-md-6-->
-				</div><!--row justify-content-md-center-->
-			</form>
-		</section><!--		container-->
+			<div class="container">
+				<div class="row justify-content-md-center mb-4">
+					<div class="col-md-12">
+						<div class="card shadow rounded contact-card">
+							<div class="card-body">
+								<div class="container">
+									<div class="row justify-content-md-center mb-4">
+										<h3 class="card-title">Contact Me</h3>
+									</div><!--row justify-content-md-center mb-4-->
+									<div class="row justify-content-md-center mb-4">
+										<div class="col-md-6">
+											<label class="control-label" for="name">Name <span class="text-danger">*</span></label>
+											<div class="input-group mb-3">
+												<div class="input-group-prepend">
+													<span class="input-group-text"><i class="fas fa-user text-primary"></i></span>
+												</div><!--input-group-prepend-->
+												<input type="text" class="form-control" id="name" placeholder="Please enter your first and last name."required />
+											</div><!--input-group mb-3-->
+										</div><!--col-md-6-->
+									</div><!--row justify-content-md-center mb-4-->
+									<div class="row justify-content-md-center mb-4">
+										<div class="col-md-6">
+											<div class="form-group mb-3">
+												<label class="control-label" for="email">Email <span class="text-danger">*</span></label>
+												<div class="input-group">
+													<div class="input-group-prepend">
+														<span class="input-group-text"><i class="fas fa-envelope text-primary"></i></span>
+													</div><!--input-group-prepend-->
+													<input type="text" class="form-control" id="email" placeholder="Please enter your email address." aria-describedby="emailHelp" required />
+												</div><!--	input-group-->
+												<small id="emailHelp" class="form-text text-muted">I'll never share your email address with anyone else.</small>
+											</div><!--	form-group mb-3-->
+										</div><!--col-md-6-->
+									</div><!--row justify-content-md-center mb-4-->
+									<div class="row justify-content-md-center mb-4">
+										<div class="col-md-6">
+											<label class="control-label" for="subject">Subject</label>
+											<div class="input-group mb-3">
+												<div class="input-group-prepend">
+													<span class="input-group-text"><i class="fas fa-tag text-primary"></i></span>
+												</div>
+												<input type="text" class="form-control" id="subject" placeholder="You may enter a subject of your message (optional)."  />
+											</div><!--input-group mb-3-->
+										</div><!--col-md-6-->
+									</div><!--row justify-content-md-center mb-4-->
+									<div class="row justify-content-md-center mb-4">
+										<div class="col-md-6">
+											<label class="control-label" for="message">Message <span class="text-danger">*</span></label>
+											<div class="input-group mb-3">
+												<div class="input-group-prepend">
+													<span class="input-group-text"><i class="fas fa-comment text-primary"></i></span>
+												</div>
+												<textarea class="form-control" rows="5" id="message" placeholder="Please enter your message here." required></textarea>
+											</div><!--input-group mb-3-->
+										</div><!--col-md-6-->
+									</div><!--row justify-content-md-center mb-4-->
+									<div class="row justify-content-md-center">
+										<div class="col-md-6">
+											<button type="button" class="btn btn-primary text-light my-3 mr-3 contact-btn"><i class="fas fa-paper-plane"></i>&nbsp;&nbsp;Send</button>
+											<button type="button" class="btn btn-warning text-light my-3 contact-btn" onclick="resetAll()"><i class="fas fa-undo"></i>&nbsp;&nbsp;Reset</button>
+										</div><!--col-md-6-->
+									</div><!--row justify-content-md-center mb-4-->
+								</div><!--container-->
+							</div><!--card-body-->
+						</div><!--card shadow rounded-->
+					</div><!--col-md-12-->
+				</div><!--row justify-content-md-center mb-4-->
+			</div><!--container-->
 		</main>
-
-		<div class="container-fluid mt-5">
-			<footer class="page-footer font-small pt-4 fixed-bottom">
-				<p class="footer-copyright text-center text-white-50 bg-dark py-2">Connect with me on LinkedIn.&nbsp&nbsp<span><a class="bootstrap-link my-footer" href="https://github.com/afnikitina"><i class="fab fa-linkedin"></i></a></span></p>
-			</footer>
-		</div>
+		<footer class="footer text-white-50 bg-dark py-2">
+			<div class="container text-center">
+				Connect with me on LinkedIn.&nbsp;&nbsp;<a class="bootstrap-link my-footer" href="https://github.com/afnikitina"><i class="fab fa-linkedin"></i></a>
+			</div>
+		</footer>
 
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
