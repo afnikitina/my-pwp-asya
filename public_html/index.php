6422779cc39bbc89f953bdc8be66bd0da4dc7cba
Asya Nikitina <anikitina@cnm.edu>
2018-08-30 15:15:17 -0600

    Update the content in index.php and deleted index_copy.php.

--- public_html/index.php
+++ public_html/index.php
@@ -120,77 +120,63 @@
 
 			<!--Contact Me Form-->
 			<div class="container">
-				<div class="row justify-content-md-center mb-4">
-					<div class="col-md-12">
-						<div class="card shadow rounded contact-card">
-							<div class="card-body">
-								<div class="container">
-									<div class="row justify-content-md-center mb-4">
+				<div class="card shadow rounded contact-card">
+					<form id="contact-form" action="php/mailer.php" method="post">
+						<div class="card-body">
+							<div class="container">
+								<div class="row justify-content-md-center my-4">
+									<div class="col-md-6">
 										<h3 class="card-title">Contact Me</h3>
-									</div><!--row justify-content-md-center mb-4-->
-									<div class="row justify-content-md-center mb-4">
-										<div class="col-md-6">
+										<div class="form-group">
 											<label class="control-label" for="name">Name <span class="text-danger">*</span></label>
-											<div class="input-group mb-3">
+											<div class="input-group">
 												<div class="input-group-prepend">
 													<span class="input-group-text"><i class="fas fa-user text-primary"></i></span>
 												</div><!--input-group-prepend-->
-												<input type="text" class="form-control" id="name" placeholder="Please enter your first and last name."required />
-											</div><!--input-group mb-3-->
-										</div><!--col-md-6-->
-									</div><!--row justify-content-md-center mb-4-->
-									<div class="row justify-content-md-center mb-4">
-										<div class="col-md-6">
-											<div class="form-group mb-3">
-												<label class="control-label" for="email">Email <span class="text-danger">*</span></label>
-												<div class="input-group">
-													<div class="input-group-prepend">
-														<span class="input-group-text"><i class="fas fa-envelope text-primary"></i></span>
-													</div><!--input-group-prepend-->
-													<input type="text" class="form-control" id="email" placeholder="Please enter your email address." aria-describedby="emailHelp" required />
-												</div><!--	input-group-->
-												<small id="emailHelp" class="form-text text-muted">I'll never share your email address with anyone else.</small>
-											</div><!--	form-group mb-3-->
-										</div><!--col-md-6-->
-									</div><!--row justify-content-md-center mb-4-->
-									<div class="row justify-content-md-center mb-4">
-										<div class="col-md-6">
+												<input type="text" class="form-control" id="name" name="name" placeholder="Please enter your first and last name."required />
+											</div><!--input-group-->
+										</div><!--form-group-->
+										<div class="form-group">
+											<label class="control-label" for="email">Email <span class="text-danger">*</span></label>
+											<div class="input-group">
+												<div class="input-group-prepend">
+													<span class="input-group-text"><i class="fas fa-envelope text-primary"></i></span>
+												</div><!--input-group-prepend-->
+												<input type="text" class="form-control" id="email" name="email" placeholder="Please enter your email address." aria-describedby="emailHelp" required />
+											</div><!--	input-group-->
+											<small id="emailHelp" class="form-text text-muted">I'll never share your email address with anyone else.</small>
+										</div><!--	form-group-->
+										<div class="form-group">
 											<label class="control-label" for="subject">Subject</label>
-											<div class="input-group mb-3">
+											<div class="input-group">
 												<div class="input-group-prepend">
 													<span class="input-group-text"><i class="fas fa-tag text-primary"></i></span>
 												</div>
-												<input type="text" class="form-control" id="subject" placeholder="You may enter a subject of your message (optional)."  />
-											</div><!--input-group mb-3-->
-										</div><!--col-md-6-->
-									</div><!--row justify-content-md-center mb-4-->
-									<div class="row justify-content-md-center mb-4">
-										<div class="col-md-6">
+												<input type="text" class="form-control" id="subject" name="subject" placeholder="You may enter a subject of your message (optional)."  />
+											</div><!--input-group-->
+										</div><!--form-group-->
+										<div class="form-group">
 											<label class="control-label" for="message">Message <span class="text-danger">*</span></label>
-											<div class="input-group mb-3">
+											<div class="input-group">
 												<div class="input-group-prepend">
 													<span class="input-group-text"><i class="fas fa-comment text-primary"></i></span>
 												</div>
-												<textarea class="form-control" rows="5" id="message" placeholder="Please enter your message here." required></textarea>
-											</div><!--input-group mb-3-->
-										</div><!--col-md-6-->
-									</div><!--row justify-content-md-center mb-4-->
-									<div class="row justify-content-md-center mb-4">
-										<div class="col-md-6">
-											<!--reCAPTCHA goes here-->
-										</div><!--col-md-6-->
-									</div><!--row justify-content-md-center mb-4-->
-									<div class="row justify-content-md-center">
-										<div class="col-md-6">
-											<button type="button" class="btn btn-primary text-light my-3 mr-3 contact-btn"><i class="fas fa-paper-plane"></i>&nbsp;&nbsp;Send</button>
-											<button type="button" class="btn btn-warning text-light my-3 contact-btn" onclick="resetAll()"><i class="fas fa-undo"></i>&nbsp;&nbsp;Reset</button>
-										</div><!--col-md-6-->
-									</div><!--row justify-content-md-center mb-4-->
-								</div><!--container-->
-							</div><!--card-body-->
-						</div><!--card shadow rounded-->
-					</div><!--col-md-12-->
-				</div><!--row justify-content-md-center mb-4-->
+												<textarea class="form-control" rows="5" id="message" name="message" placeholder="Please enter your message here." required></textarea>
+											</div><!--input-group-->
+										</div><!--form-group-->
+										<div class="form-group">
+											<div class="g-recaptcha" data-sitekey="6Lekam0UAAAAAHZRc3RfTjQIa0XtezD-UUzyBegy"></div>
+										</div><!--form-group-->
+										<div class="form-group">
+											<button type="button" class="btn btn-primary text-light contact-btn mr-3"><i class="fas fa-paper-plane"></i>&nbsp;&nbsp;Send</button>
+											<button type="button" class="btn btn-warning text-light contact-btn" onclick="resetAll()"><i class="fas fa-undo"></i>&nbsp;&nbsp;Reset</button>
+										</div><!--form-group-->
+									</div><!--	col-md-6-->
+								</div><!--row justify-content-md-center mb-4-->
+							</div><!--container-->
+						</div><!--card-body-->
+					</form>
+				</div><!--card shadow rounded contact-card	-->
 			</div><!--container-->
 		</main>
 		<footer class="footer text-white-50 bg-dark py-2">
@@ -199,9 +185,24 @@
 			</div>
 		</footer>
 
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+		<!-- jQuery Form, Validate, Additional Methods -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js" integrity="sha256-2Pjr1OlpZMY6qesJM68t2v39t+lMLvxwpa8QlRjJroA=" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js" integrity="sha256-F6h55Qw6sweK+t7SiOJX+2bpSAa3b/fnlrVCJvmEj1A=" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js" integrity="sha256-0Yg/eibVdKyxkuVo1Qwh0DspoUCHvSbm/oOoYVz32BQ=" crossorigin="anonymous"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- form validate js -->
+
+
+		<!-- custom js -->
 		<script src="js/script.js" type="application/javascript"></script>
 	</body>
 </html>
